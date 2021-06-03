c54a3a95c462318ee44cf6610a2f948b9651561d
test <test@test.com>
2021-06-03 11:50:50 +0900

    3교시 종료 커밋

--- src/test/java/com/edu/test/DataSourceTest.java
+++ src/test/java/com/edu/test/DataSourceTest.java
@@ -1,5 +1,11 @@
 package com.edu.test;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.inject.Inject;
+import javax.sql.DataSource;
+
 import org.apache.log4j.Logger;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -22,7 +28,22 @@ import org.springframework.test.context.web.WebAppConfiguration;
 public class DataSourceTest {
 	//디버그용 로그 객체변수 생성
 	private Logger logger = Logger.getLogger(DataSourceTest.class);
-	
+	//dataSource 객체는 데이터베이스 객체를 pool로 저장해서 사용할 때 DataSource 클래스를 사용(아래)
+	@Inject //인젝트는 스프링에서 객체를 만드는 방법, 이전 자바에서는 new 키워드로 객체를 만들었음.
+	DataSource dataSource; //Inject로 객체를 만들면 메모리 관리를 스프링이 대신해 줌.
+	// 자바8부터 Inject 지원. 이전 버전 자바에서는 @Autowired 로 객체를 생성.
+	@Test
+	public void dbConnectionTest() {
+		//데이터베이스 커넥션 테스트용: 설정은 root-context의 빈(스프링클래스)를 이용
+		try {
+			Connection connection = dataSource.getConnection();
+			logger.debug("데이터 베이스 접속 성공. DB종류는" + connection.getMetaData().getDatabaseProductName());
+		} catch (SQLException e) {
+			logger.debug("데이터 베이스 접속 실패.");
+			//e.printStackTrace();
+		}
+		
+	}
 	@Test
 	public void junitTest() {
 		//로거는 장점>조건에 따라서 출력을 조정할 수 있음.
