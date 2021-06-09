86d070a47d768658524114a9fccf7140a4c46db3
test <test@test.com>
2021-06-09 11:17:18 +0900

    3교시 중간커밋

--- src/main/java/com/edu/dao/IF_MemberDAO.java
+++ src/main/java/com/edu/dao/IF_MemberDAO.java
@@ -14,4 +14,6 @@ public interface IF_MemberDAO {
 	//List<제네릭타입> : MemberVO 1개 레코드를 List 클래스형으로 감싸줌.
 	//다수의 레코드를 저장할 수 있게 됨.
 	public List<MemberVO> selectMember() throws Exception;
+	//회원 전체 수를 구함(아래)
+	public int countMember() throws Exception; 
 }
