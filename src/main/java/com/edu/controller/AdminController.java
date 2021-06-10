b8d87d73c5a797bbfc04c6cab8d67cd1e58f89db
yush <yush@DESKTOP-4UCLJN5>
2021-06-10 15:49:01 +0900

    6교시 종료 커밋

--- src/main/java/com/edu/controller/AdminController.java
+++ src/main/java/com/edu/controller/AdminController.java
@@ -5,6 +5,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.edu.vo.PageVO;
+
 /**
  * 이 클래스는 Admin(관리자단)을 접근하는 클래스.
  * 변수(Object)를 만들어서 jsp로 전송 + jsp 값을 받아서 Object로 처리
@@ -13,12 +15,12 @@ import org.springframework.web.bind.annotation.RequestMethod;
  */
 @Controller
 public class AdminController {
-	//이 메서드는 회원목록을 출력하는 jsp와 매핑이 됩니다.
-	@RequestMapping(value="/admin/board/board_list", method=RequestMethod.GET)
-	public String selectMember() throws Exception {
-		
-		return "admin/board/board_list";//jsp파일 상대경로
-	}
+   //이 메소드는 회원목록을 출력하는 jsp와 매핑이 됩니다.
+   @RequestMapping(value="/admin/member/member_list", method=RequestMethod.GET)
+   public String selectMember(PageVO pageVO) throws Exception {
+      
+      return "admin/member/member_list"; //jsp파일 상대경로
+   }
 	// @RequestMapping 에서는 반드시 URL을 절대경로로 표기
 	@RequestMapping(value="/admin", method =RequestMethod.GET)
 	public String admin(Model model) throws Exception{//에러 발생시 Exception을 스프링으로 보내게 됩니다.
