4bc7170e3a9e03e68beec1cfdb159d47370ef17c
yush <yush@DESKTOP-4UCLJN5>
2021-06-10 16:35:11 +0900

    7교시 종료커밋

--- src/main/java/com/edu/controller/AdminController.java
+++ src/main/java/com/edu/controller/AdminController.java
@@ -1,10 +1,18 @@
 package com.edu.controller;
 
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.edu.service.IF_MemberService;
+import com.edu.vo.MemberVO;
 import com.edu.vo.PageVO;
 
 /**
@@ -15,12 +23,27 @@ import com.edu.vo.PageVO;
  */
 @Controller
 public class AdminController {
-   //이 메소드는 회원목록을 출력하는 jsp와 매핑이 됩니다.
-   @RequestMapping(value="/admin/member/member_list", method=RequestMethod.GET)
-   public String selectMember(PageVO pageVO) throws Exception {
-      
-      return "admin/member/member_list"; //jsp파일 상대경로
-   }
+	//디버그용 로그 객제 생성
+	private Logger logger = LoggerFactory.getLogger(AdminController.class);
+    //이 메소드는 회원목록을 출력하는 jsp와 매핑이 됩니다.
+	@Inject
+	   private IF_MemberService memberService;
+	   
+	@RequestMapping(value="/admin/member/member_list", method=RequestMethod.GET)
+		public String selectMember(PageVO pageVO) throws Exception {
+	      //jsp 검색시 search_type,search_keyword로 내용이 PageVO클래스에 Set 됩니다.
+	      
+	      //역방향: 검색한 결과를 jsp로 보내줍니다.
+	      if(pageVO.getPage() == null) {
+	         pageVO.setPage(1);
+	      }
+	      //pageVO의 calcPage메서드를 실행하려면, 필수 변수 값 입력(아래)
+	      pageVO.setQueryPerPageNum(10);
+	      logger.info("디버그"+pageVO.toString());
+	      List<MemberVO> listMember = memberService.selectMember(pageVO);
+	      pageVO.setTotalCount(listMember.size());//검색이 되든 되지 않든 결과는 전체 카운트 값.
+	      return "admin/member/member_list"; //jsp파일 상대경로
+	   }
 	// @RequestMapping 에서는 반드시 URL을 절대경로로 표기
 	@RequestMapping(value="/admin", method =RequestMethod.GET)
 	public String admin(Model model) throws Exception{//에러 발생시 Exception을 스프링으로 보내게 됩니다.
