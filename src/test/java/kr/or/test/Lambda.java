4236c3f7516fcba7e4f18cc71e13b9c54bc9c048
csy5770 <yush60@gmail.com>
2021-07-01 14:06:12 +0900

    lambda 시작 전

---
+++ src/test/java/kr/or/test/Lambda.java
@@ -0,0 +1,16 @@
+package kr.or.test;
+/**
+ * 이 클래스는 람다식(애로우 메서드)을 테스트하는 클래스.
+ * @author csy5770
+ *
+ */
+public class Lambda {
+
+	//스태틱 메서드는 클래스를 실행하면, 메모리에 등록 되는 메서드를 뜻함.
+	//스태틱을 지정하지 않으면, 런타임 시 메모리에 등록 됨.
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
